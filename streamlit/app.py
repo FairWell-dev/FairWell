7e4deb98d354b8e5cd4cb5380acc26d0e4d1cb4f
NG Rui Qin <ruiqin.ng.2016@sis.smu.edu.sg>
2021-10-22 18:12:34 +0800

    Add file upload feature

--- streamlit/app.py
+++ streamlit/app.py
@@ -13,17 +13,30 @@ st.set_page_config(page_title='Responsible AI Toolkit',
 page = st.sidebar.radio('Page', 
                         options=['Feature Explorer'], 
                         index=0)
-dataset = st.sidebar.text_input('Dataset', 
-                                value='NYC Subway.csv')
+st.sidebar.markdown('**Select or Upload a Dataset**')
+
+def update_selectbox(file):
+    selectbox_options = [file.name]
+    selectbox_idx = len(selectbox_options) - 1
+
+dataset = st.sidebar.selectbox('Dataset', 
+                               options=['NYC Subway'],
+                               index=0)
+
+file = st.sidebar.file_uploader('Upload Dataset (CSV)', type=['csv'])
+
 dataset = 'diamonds'  # To remove after we preprocess our subway dataset
 
 # Title
 st.title('Responsible AI Toolkit')
 
 # Extract data
-df = sns.load_dataset(dataset)
+if file is None:
+    df = sns.load_dataset(dataset)
+else:
+    df = pd.read_csv(file)
 
 if page.lower() == 'feature explorer':
-      features_page.render(df)
+    features_page.render(df)
 else:
-      st.text('Page ' + page + ' is not implemented.')
+    st.text('Page ' + page + ' is not implemented.')
